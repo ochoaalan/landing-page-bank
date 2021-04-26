292363617a6c0d13266844dd17ed81794067f14a
ochoaalan <alan1192@gmail.com>
2021-04-26 06:18:19 -0500

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  //if header doesn't have open class add it, if it does remove it
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open'); 
+    fadeElements.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+    else { // Open Hamburger Menu
+      body.classList.add('noscroll');
+      header.classList.add('open');
+      fadeElements.forEach(function(element){
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+      });
+  
+    }  
+  });
